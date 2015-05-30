79a483f1b2aa02bf4af1a91cc17cc92995c19399
Ashkan Karimdaidi <ashkan.karimdaidi@icloud.com>
2015-05-30 19:08:38 +0200

    initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((.*r . ;; This applies these settings regardless of major mode
+      ((pony-settings (make-pony-project
+                       :python "/Users/ashkankarimdaidi/.virtualenvs/dj_test/bin/python"
+                       :pythonpath "/Users/ashkankarimdaidi/workspace/superlist"
+                       :settings "settings")
+                      )
+       )
+      )
+ )
