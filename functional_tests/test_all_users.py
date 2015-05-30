be22fce4a1fed9625ff9d933d722bff38b1e5ca5
Ashkan Karimdaidi <ashkan.karimdaidi@icloud.com>
2015-05-30 19:54:41 +0200

    short pice of functional test

---
+++ functional_tests/test_all_users.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+
+
+
+
+if __name__ == '__main__':
+	pass
